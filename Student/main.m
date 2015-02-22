dd6efd014f3be10a66e7fc722a781de5a19994e1
Theodore <theodore_wu@live.com.sg>
2015-02-22 14:14:19 +0800

    first commit

---
+++ Student/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Student
+//
+//  Created by Theodore  on 22/2/15.
+//  Copyright (c) 2015 Theodore . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
